b236ae512cadf4cf526381ba820b2b72bb150699
Abu-safyan yusuf <abusufyanyusuf080@gmail.com>
2023-10-06 20:49:29 +0100

    adding animations

--- index.js
+++ index.js
@@ -45,3 +45,45 @@ window.addEventListener('scroll', shadowHeader)
 // sr.reveal(` .favorites, .content__text, .explore__img, nav__right`, {origin: 'left'})
 // sr.reveal(`.home__text, .oval, .about__intro`, {origin: 'top'})
 // sr.reveal(`.about__content, .connect, .form`, {origin: 'bottom'})
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/*HOME*/
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', {delay: 200})
+sr.reveal('.home__text', {origin:'left', delay: 400})
+sr.reveal('.oval', {origin:'right', delay: 400})
+
+/*OUR FAVE*/
+sr.reveal('.about__img', {delay: 200})
+sr.reveal('.favorites__text', {origin:'left', delay: 400})
+sr.reveal('.favorites', {origin:'bottom', delay: 400})
+sr.reveal('.about__content', {origin:'top', delay: 400})
+sr.reveal('.about__intro', {origin:'bottom', delay: 600, delay: 400})
+sr.reveal('.explore__text', {origin:'left', delay: 400})
+sr.reveal('.explore__img', {origin:'right', delay: 400})
+sr.reveal('.blog__container-left', {origin:'left', delay: 400})
+sr.reveal('.contact', {origin:'bottom', delay: 400})
+
+
+
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
+sr.reveal('.skills__img', {delay: 400})
+
+/*SCROLL PORTFOLIO*/
+sr.reveal('.portfolio__img', {interval: 200})
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__input', {delay: 400})
+sr.reveal('.contact__button', {delay: 600})
+sr.reveal('.contact__form', {delay: 600})
